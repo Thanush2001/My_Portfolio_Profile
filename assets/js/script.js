c3c0665f07770180b2e77f056746af6853b3a02b
THANUSH V <114146514+Thanush2001@users.noreply.github.com>
2025-03-17 22:21:46 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Frontend development", "Backend development", "Web designing", "Python development", "Full Stack development"],
+    strings: ["Frontend development", "Backend development","Data Analyst","Web designing", "Python development", "Full Stack development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
